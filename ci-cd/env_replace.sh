ce858b2239fa5285ef73882520629c58b13522a2
Alex Krumov <alex.krumov@bundlearrows.com>
2023-11-09 08:54:46 +0200

    Update env_replace.sh scipt

--- ci-cd/env_replace.sh
+++ ci-cd/env_replace.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
+#!/bin/sh
 DIST_DIR=/app
-vue_app_vars=()
-
-# Iterate through environment variables
-for var in $(env | grep '^VITE_'); do
-    vue_app_vars+=("$var")
-done
 
 # Define a function to replace variables in a file
 replace_variables() {
     local file="$1"
-    for var in "${vue_app_vars[@]}"; do
+    for var in "$(env | grep '^VITE_')"; do
         local var_name="${var%%=*}"
         local var_value="${var#*=}"
         sed -i "s#${var_name}_REPLACE#$var_value#g" "$file"
